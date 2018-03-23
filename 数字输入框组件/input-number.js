1472e1ae7296770fa8b7ccc59d35d0384a5ff125
Yang Yi <534650291@qq.com>
2018-03-23 19:49:49 +0800

    初步完成

--- 数字输入框组件/input-number.js
+++ 数字输入框组件/input-number.js
@@ -1,3 +1,81 @@
+function isValueNumber(value){
+	return (/(^-[0-9]+\.{1}\d+$)|(^-?[1-9][0-9]*$)|(^-?0{1}$)/).test(value+"");
+}
 Vue.component("input-number",{
-	
+	template:'\
+	<div class="input-number">\
+	<input\
+		type="text"\
+		:value="currentValue"\
+		@change="handleChange">\
+		<button\
+			@click="handleDown"\
+			:disabled="currentValue<=min">-</button>\
+		<button\
+			@click="handleUp"\
+			:disabled="currentValue>=max">+</button>\
+	</div>',
+	props:{
+		max:{
+			type:number,
+			default:Infinity
+		},
+		min:{
+			type:number,
+			default:Infinity
+		},
+		value:{
+			type:number
+			value:0
+		}
+	},
+	data:function(){
+		return{
+			currentValue:this.value
+		}
+	},
+	watch:{
+		currentValue:function(val){
+			this.$emit("input",val);
+			this.$emit("on-change",val);
+		},
+		value:function(val){
+			this.updateValue(val);
+		}
+	},
+	methods:{
+		handleUp:function(){
+			if(this.currentValue>=this.max) return;
+			this.currentValue+=1;
+		}
+		handleDown:function(){
+			if (this.currentValue<=this.min) return;
+			this.currentValue-=1;
+		}
+		updateValue:function(val){
+			if(val>this.max) val=this.max;
+			if(val<this.min) val=this.min;
+			this.currentValue=val;
+		},
+		handleChange:function(event){
+			var val=event.target.value.trim();
+			var max=this.max;
+			var min=this.min;
+			if(isValueNumber(val)){
+				val=Number(val);
+				this.currentValue=val;
+				if(val>max)
+					this.currentValue=max;
+				else if (val<min) {
+					this.currentValue=min;
+				}
+				else{
+					event.target.value=this.currentValue;
+				}
+			}
+		}
+	},
+	mounted:function(){
+		this.updateValue(this.value);
+	}
 })
